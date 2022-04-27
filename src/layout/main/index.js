5b410f803d8267b2544f4371f52210ca4c500ca5
Tsogt <Tsogt@LEAP-23s-iMac.local>
2022-04-27 18:10:30 +0800

    this commit has the MainLayout file set up for the header and the footer. The header has also been made. Only thing left to compleat the header is the css.

--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -1,2 +1,5 @@
 export * from './header'
+<<<<<<< HEAD
 export * from './footer'
+=======
+>>>>>>> this commit has the MainLayout file set up for the header and the footer. The header has also been made. Only thing left to compleat the header is the css.
