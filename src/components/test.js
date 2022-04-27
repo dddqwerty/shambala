423ec14d0b0a901033931061e4b3ecff0ef6f392
Dorjsuren <dddqwerty@Dorjsurens-MacBook-Pro.local>
2022-04-27 16:19:55 +0800

    demo template

---
+++ src/components/test.js
@@ -0,0 +1,5 @@
+const HelloWorld = () => {
+  return <h1 className="text-yellow-300">Hello World</h1>
+}
+
+export default HelloWorld
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { Hello } from 'components'
+
+export default function Home() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  )
+}
