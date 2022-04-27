a3af14bfab1eecc5c3387f1cdd06e9c4e689d8da
tsengegimnida <tsengeg65@gmail.com>
2022-04-27 19:10:29 +0800

    color hiiv

---
+++ src/theme/colors.js
@@ -0,0 +1,19 @@
+module.exports = {
+    primary: {
+        'white': '#FFFFFF',
+        'yellow': '#FFD217',
+    },
+    secondary: {
+        'blue': "#007AFF",
+    },
+    textColor: {
+        secondary: {
+            'light-gray': '#F3F4F5',
+            'dark-gray': "#2A2C2C",
+        }
+    },
+    backgroundColor: {
+        'dark-gray': "#202121",
+
+    }
+}
