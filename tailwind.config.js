ad947a422cc48802ad827a2d5034118d35a04ee3
tsengegimnida <tsengeg65@gmail.com>
2022-04-27 19:10:29 +0800

    color hiiv

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const colors = require('./src/theme/colors')
 module.exports = {
   content: ['./src/**/**/*.{js,ts,jsx,tsx}'],
   important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          white: colors.primary.white,
+          yellow: colors.primary.yellow
+        },
+        secondary: {
+          blue: colors.secondary.blue,
+        },
+        textColor: {
+          secondary: {
+            lightGray: colors.textColor.secondary['light-gray'],
+            darkGray: colors.textColor.secondary['dark-gray'],
+          }
+        },
+        backgroundColor: {
+          darkGray: colors.backgroundColor['dark-gray'],
+        }
+      },
+    },
   },
   plugins: [],
 }
