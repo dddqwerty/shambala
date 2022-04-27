ada623a2a7328fb82680ca7f1170ae1af93adc9e
Tsogt <Tsogt@LEAP-23s-iMac.local>
2022-04-27 20:11:14 +0800

    just a short little push to transfer to home computer.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import { Hello, Typography } from 'components'
 import { MainLayout } from 'layout'
-
+import { handleIcon } from 'utils'
 export default function Home() {
   return (
     <div>
       <Typography variant="h1">dwqdwq</Typography>
       <Hello />
       <MainLayout />
+      {handleIcon({
+        icon: 'facebook',
+        size: 40,
+        className: 'text-primary-yellow',
+      })}
     </div>
   )
 }
