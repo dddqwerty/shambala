fd21f9bf72c3b338e6d899e79360bcc7d06d6d55
he1ooooo <munkhbaatarbilguun50@gmail.com>
2022-04-29 19:05:03 +0800

    added button comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Typography variant="body-mobile">Heading 1</Typography>
+      <Typography variant="body-web">Heading 2</Typography>
     </div>
   )
 }
