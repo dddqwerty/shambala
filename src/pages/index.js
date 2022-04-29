1764e7f9b9e962334c028742c51b415e61e28197
TsogtOnCrack <tsogt.otgo@gmail.com>
2022-04-29 22:28:13 +0800

    god damn it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
-import { MainLayout } from 'layout'
+import { Typography } from 'components'
+
 export default function Home() {
   return (
     <div>
-      <MainLayout></MainLayout>
+      <Typography variant="body-mobile">Heading 1</Typography>
+      <Typography variant="body-web">Heading 2</Typography>
     </div>
   )
 }
