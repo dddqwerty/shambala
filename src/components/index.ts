fb667bb842bdc4c76312241b8ded6381069c1040
he1ooooo <92196392+he1ooooo@users.noreply.github.com>
2022-04-29 19:07:39 +0800

    added button comp (#4)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Typography } from './typography'
+export { default as Button } from './Button'
--- src/pages/style/index.jsx
+++ src/pages/style/index.jsx
@@ -1,4 +1,5 @@
 import { Typography } from 'components'
+import Link from 'next/link'
 
 export default function Home() {
   return (
