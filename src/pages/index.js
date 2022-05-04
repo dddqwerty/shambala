03791b3981896e98574c577fab6405e2c2dd56dc
Dorjsuren <dddqwerty@Dorjsurens-MacBook-Pro.local>
2022-05-04 16:17:24 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import { MainLayout } from 'layout'
 export default function Home() {
   return (
     <div>
-      <MainLayout></MainLayout>
+      <MainLayout>
+        <div className="w-[500px] h-[500px] bg-red-800" />
+      </MainLayout>
     </div>
   )
 }
