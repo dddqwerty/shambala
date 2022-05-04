aaab742ee82a40f8f0b3e76a7c02527197221894
Honi <93262355+Honi3211@users.noreply.github.com>
2022-05-04 20:13:25 +0800

    memberCard component (#11)
    * honey

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export { default as Typography } from './typography'
 export { default as Button } from './Button'
 export { default as Card } from './card'
 export { default as GoalCard } from './goalCard'
-export * from "./big"
-export * from "./small"
+export {default as MemberCard} from './memberCard'
