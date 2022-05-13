68fd690dd187e9a9acb6cdb722bd977bed745d4a
Dorjsuren <dddqwerty@Dorjsurens-MacBook-Pro.local>
2022-05-13 16:08:23 +0800

    added animation examples

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import { MainLayout } from 'layout'
-
 import Fournumber from 'components/telmun/hedentoo'
+
 export default function Home() {
   return (
     <div>
       <MainLayout classname={'bg-backgroundColor-darkGray'}>
-        <div className="h-[500px] w-[500px] bg-primary-yellow" />
-
+        <div className="h-[2000px] w-[500px] bg-primary-yellow" />
         <Fournumber />
       </MainLayout>
     </div>
