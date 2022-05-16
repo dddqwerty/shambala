d1a0de3854ffd94d007906d3b66b9f5c55b70a2b
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2022-05-16 19:36:10 +0800

    nnn

--- src/components/cards/AnimatedCard.tsx
+++ src/components/cards/AnimatedCard.tsx
@@ -10,39 +10,34 @@ export const Member = () => {
   return (
     <div className="ml-20 z-50 h-[256px] w-[218px]">
       <div className="flex flex-col gap-3 overflow-hidden relative">
-        <motion.div
-          animate={animControls}
-          className="right-[596px] relative bg-blue-600 h-[1000px] w-24 z-50 rotate-45"
-          onAnimationComplete={() => setAnim(false)}
-        ></motion.div>
         <div
-          className="absolute bg-white w-[256px] h-[218px] rounded-md transform"
-          onMouseEnter={() => {
-            if (!anim) {
-              setAnim(true)
-              animControls
-                .start({
-                  x: 398,
-                  y: -292,
-                  rotate: 222,
-                  transition: { duration: 0.05, type: 'tween' },
-                })
-                .catch(console.error)
-            }
-          }}
-          onMouseLeave={() => {
-            setAnim(false)
-            animControls
-              .start({
-                x: -999,
-                y: 99,
-                rotate: -45,
-                transition: { duration: 1, type: 'tween' },
-              })
-              .catch(console.error)
-          }}
+          className="relative bg-white w-[256px] h-[218px] rounded-md transform overflow-hidden"
+          // onMouseEnter={() => {
+          //   if (!anim) {
+          //     setAnim(true)
+          //     animControls
+          //       .start({
+          //         x: 0,
+          //         y: 0,
+          //         rotate: -45,
+          //         transition: { duration: 0.3, type: 'tween' },
+          //       })
+          //       .catch(console.error)
+          //   }
+          // }}
+          // onMouseLeave={() => {
+          //   setAnim(false)
+          //   animControls
+          //     .start({
+          //       x: 10,
+          //       y: 0,
+          //       rotate: -45
+            
+          //     })
+          //     .catch(console.error)
+          // }}
         >
-          <Image
+          {/* <Image
             src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg2%2F20090811%2Fugly_people_23.jpg&f=1&nofb=1"
             alt="CEO"
             width="256px"
@@ -50,9 +45,28 @@ export const Member = () => {
             layout="fixed"
             objectFit="fill"
             quality={100}
+          /> */}
+
+          <motion.div
+            animate={animControls}
+            initial={{x: 0, y: 0}}
+            className="absolute -left-[35%] -top-10 bg-blue-600  z-50 border-b-[30px] border-l-[150px] border-r-[150px] border-red"
+            onAnimationComplete={() => setAnim(false)}
           />
+          {/* <motion.div
+            animate={animControls}
+            // initial={{x: 10}}
+            className="absolute -right-[20%] -bottom-10 bg-blue-600 h-[60%] w-[100%] z-50 -rotate-45"
+            onAnimationComplete={() => setAnim(false)}
+          /> */}
+          {/* <motion.div
+            animate={animControls}
+            className="absolute right-[596px] bg-blue-600 h-[100px] w-24 z-50 rotate-45"
+            onAnimationComplete={() => setAnim(false)}
+          /> */}
         </div>
-        <div className="flex flex-col gap-2 mt-4">
+
+        {/* <div className="flex flex-col gap-2 mt-4">
           <div className="">
             <Typography variant="p-small" className="text-white opacity-40 text-lg font-normal">
               Шижирбат
@@ -64,7 +78,7 @@ export const Member = () => {
           <Typography variant="h2" className="text-white opacity-80 !text-xs">
             CEO
           </Typography>
-        </div>
+        </div> */}
       </div>
     </div>
   )
