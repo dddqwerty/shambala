74fa8e84399d2af0ae7e6c2393690ecba54a22a9
Honi <93262355+Honi3211@users.noreply.github.com>
2022-05-16 20:06:17 +0800

    index nemelt (#23)
    * honey

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,9 @@ module.exports = {
       boxShadow: {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
+      animation: {
+        'spin-slow': 'spin 40s linear infinite',
+      },
     },
   },
   plugins: [],
