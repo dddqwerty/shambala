5c5d39db83e07a0264e8fafcd656f658a41e6944
tsengegimnida <89061311+tsengegimnida@users.noreply.github.com>
2022-05-16 20:48:22 +0800

    globe card ba bonus gehde bonus n hagas (#24)
    * color hiiv
    
    * globe icontoi caard/ what are we doing

---
+++ src/components/globeCard/buurunhiinuud.tsx
@@ -0,0 +1,23 @@
+const Buurunhii = () => {
+
+return(
+    <>
+        <div className="hidden mobile:inline mobile:z-10 absolute mobile:-left-[27%] mobile:-top-[-11%] opacity-30">
+            <img src="/assets/ellipse/araiTom.svg" className="max-w-full" alt="" />
+        </div>
+        <div className=" hidden mobile:inline absolute z-10 -right-[54%] -top-[-46%] w-[70%] md:right-[8%] md:top-[-5%] md:w-[19%] opacity-30">
+            <img src="/assets/ellipse/araiJijig.svg" className="max-w-full" alt="" />
+        </div>
+        <div className="hidden mobile:inline absolute  top-[35%] left-[31%]">
+            <img src="/assets/ellipse/ellipse56.svg" alt="" />
+        </div>
+        <div className="hidden mobile:inline absolute  top-[20%] right-[8%]">
+            <img src="/assets/ellipse/ellipse56.svg" alt="" />
+        </div>
+        <div className="hidden mobile:inline absolute  top-[49%] right-[17.5%]">
+            <img src="/assets/ellipse/ellipse57.svg" alt="" />
+        </div>
+</>
+    )
+}
+export default Buurunhii
