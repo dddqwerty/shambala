4f171b759aff8e25aef6046fd7c3a9b195af1a9f
tsengegimnida <89061311+tsengegimnida@users.noreply.github.com>
2022-05-16 20:48:22 +0800

    globe card ba bonus gehde bonus n hagas (#24)
    * color hiiv
    
    * globe icontoi caard/ what are we doing

---
+++ src/components/bonus/deedtal.tsx
@@ -0,0 +1,15 @@
+import Typography from "components/typography";
+const GarchigBonus = () => {
+    return (
+
+        <div className=" flex items-center">
+            <Typography variant="h3" className="text-primary-white">
+                <p> Price per tree </p>
+            </Typography>
+            <Typography variant="h2" className="text-primary-white">
+                <p> 80,000 MNT </p>
+            </Typography>
+        </div>
+    )
+}
+export default GarchigBonus
