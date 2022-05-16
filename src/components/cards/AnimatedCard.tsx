93eee3b175adc99941d0f0c2c262dec40854a107
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2022-05-16 18:40:05 +0800

    hm

--- src/components/cards/AnimatedCard.tsx
+++ src/components/cards/AnimatedCard.tsx
@@ -1,27 +1,58 @@
 import * as React from 'react'
 import Image from 'next/image'
 import Typography from 'components/typography'
+import { motion, useAnimation } from 'framer-motion'
 
 export const Member = () => {
-  /**HAHSHdaushdiashdiuashdigawiudkahbsdiauhwjdb */
-  return (
-    <div className="ml-20 z-50 overflow-hidden h-[256px] w-[218px]">
-      <div className="flex flex-col gap-3 overflow-hidden relative">
-        <div className="right-64 relative bg-blue-600 h-[200px] w-24 z-50 rotate-45" onMouseEnter={() => {
+  const [anim, setAnim] = React.useState(false)
+  const animControls = useAnimation()
 
-        }}></div>
-        <div className="absolute bg-white w-[256px] h-[218px]"></div>
-        {/* <Image
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg2%2F20090811%2Fugly_people_23.jpg&f=1&nofb=1"
-        alt="CEO"
-        width="218px"
-        height="256px"
-        layout="fixed"
-        className="rounded-md transition hover:scale-105"
-        objectFit="fill"
-        quality={100}
-      /> */}
-        <div className="flex flex-col gap-2">
+  return (
+    <div className="ml-20 z-50 h-[256px] w-[218px]">
+      <div className="flex flex-col gap-3 overflow-hidden relative">
+        <motion.div
+          animate={animControls}
+          className="right-[596px] relative bg-blue-600 h-[1000px] w-24 z-50 rotate-45"
+          onAnimationComplete={() => setAnim(false)}
+        ></motion.div>
+        <div
+          className="absolute bg-white w-[256px] h-[218px] rounded-md transform"
+          onMouseEnter={() => {
+            if (!anim) {
+              setAnim(true)
+              animControls
+                .start({
+                  x: 398,
+                  y: -292,
+                  rotate: 222,
+                  transition: { duration: 0.05, type: 'tween' },
+                })
+                .catch(console.error)
+            }
+          }}
+          onMouseLeave={() => {
+            setAnim(false)
+            animControls
+              .start({
+                x: -999,
+                y: 99,
+                rotate: -45,
+                transition: { duration: 1, type: 'tween' },
+              })
+              .catch(console.error)
+          }}
+        >
+          <Image
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg2%2F20090811%2Fugly_people_23.jpg&f=1&nofb=1"
+            alt="CEO"
+            width="256px"
+            height="218px"
+            layout="fixed"
+            objectFit="fill"
+            quality={100}
+          />
+        </div>
+        <div className="flex flex-col gap-2 mt-4">
           <div className="">
             <Typography variant="p-small" className="text-white opacity-40 text-lg font-normal">
               Шижирбат
@@ -30,7 +61,7 @@ export const Member = () => {
               Бямбасүрэн
             </Typography>
           </div>
-          <Typography variant="h2" className="text-white !text-p2 opacity-80 text-xs">
+          <Typography variant="h2" className="text-white opacity-80 !text-xs">
             CEO
           </Typography>
         </div>
