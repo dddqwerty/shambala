9bce70a75d8ea2d8cd2c840aa49d2b5548456dc7
TsogtOnCrack <tsogt.otgo@gmail.com>
2022-05-17 22:06:03 +0800

    pls be done

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,16 +56,17 @@ module.exports = {
       },
       width: {
         planWidth: '100%',
-      boxShadow: {
-        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-      },
-      animation: {
-        'spin-slow': 'spin 40s linear infinite',
-        width: {
-          planWidth: '100%',
+        boxShadow: {
+          '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        },
+        animation: {
+          'spin-slow': 'spin 40s linear infinite',
+          width: {
+            planWidth: '100%',
+          },
         },
       },
+      plugins: [],
     },
-    plugins: [],
   },
 }
