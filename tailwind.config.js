7900b7ea152e2c3d7cc8a68f43aa174a354b4265
Tsogt <Tsogt@LEAP-23s-iMac.local>
2022-05-18 19:42:24 +0800

    Everything is almost done, only animations left.

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,12 @@ module.exports = {
       maxWidth: {
         shambWidth: '1200px',
       },
+      width: {
+        planWidth: '100%',
+      },
+      height: {
+        lineHeight: '450px',
+      },
       boxShadow: {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
