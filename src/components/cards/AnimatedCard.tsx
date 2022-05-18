c0d993a040df817db03a888ea56ca9356983546f
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2022-05-18 20:19:21 +0800

    bru

--- src/components/cards/AnimatedCard.tsx
+++ src/components/cards/AnimatedCard.tsx
@@ -1,41 +1,89 @@
 import * as React from 'react'
-import Image from 'next/image'
-import Typography from 'components/typography'
 import { motion, useAnimation } from 'framer-motion'
+import Image from 'next/image'
 
 export const Member = () => {
   const [anim, setAnim] = React.useState(false)
-  const animControls = useAnimation()
+  const [animControls] = React.useState([useAnimation(), useAnimation(), useAnimation(), useAnimation()]) // very sloppy... too bad
 
   return (
     <div className="ml-20 z-50 h-[256px] w-[218px]">
       <div className="flex flex-col gap-3 overflow-hidden relative">
         <div
-          className="relative bg-white w-[256px] h-[218px] rounded-md transform overflow-hidden"
-          // onMouseEnter={() => {
-          //   if (!anim) {
-          //     setAnim(true)
-          //     animControls
-          //       .start({
-          //         x: 0,
-          //         y: 0,
-          //         rotate: -45,
-          //         transition: { duration: 0.3, type: 'tween' },
-          //       })
-          //       .catch(console.error)
-          //   }
-          // }}
-          // onMouseLeave={() => {
-          //   setAnim(false)
-          //   animControls
-          //     .start({
-          //       x: 10,
-          //       y: 0,
-          //       rotate: -45
-            
-          //     })
-          //     .catch(console.error)
-          // }}
+          className="relative bg-white w-[256px] h-[218px] rounded-md transform overflow-hidden transition hover:bg-gray-800"
+          onMouseEnter={() => {
+            if (!anim) {
+              setAnim(true)
+              animControls[0]
+                .start({
+                  x: 0,
+                  y: 0,
+                  rotate: -40,
+                  transition: { duration: 0.07, type: 'tween' },
+                })
+                .catch(console.error)
+              animControls[1]
+                .start({
+                  x: 0,
+                  y: 0,
+                  rotate: -45,
+                  transition: { duration: 0.07, type: 'tween' },
+                })
+                .catch(console.error)
+
+              animControls[2]
+                .start({
+                  x: -110,
+                  y: -15,
+                  rotate: -45,
+                  transition: { duration: 0.2, type: 'tween' },
+                })
+                .catch(console.error)
+              animControls[3]
+                .start({
+                  x: -100,
+                  y: -15,
+                  rotate: -45,
+                  transition: { duration: 0.2, type: 'tween' },
+                })
+                .catch(console.error)
+            }
+          }}
+          onMouseLeave={() => {
+            setAnim(false)
+            animControls[0]
+              .start({
+                x: 100,
+                y: 100,
+                rotate: -30,
+                transition: { duration: 0.4, type: 'tween' },
+              })
+              .catch(console.error)
+            animControls[1]
+              .start({
+                x: -100,
+                y: -100,
+                rotate: -45,
+                transition: { duration: 0.4, type: 'tween' },
+              })
+              .catch(console.error)
+            animControls[2]
+              .start({
+                x: -100,
+                y: 100,
+                rotate: -45,
+                transition: { duration: 0.4, type: 'tween' },
+              })
+              .catch(console.error)
+            animControls[3]
+              .start({
+                x: 100,
+                y: -100,
+                rotate: -45,
+                transition: { duration: 0.4, type: 'tween' },
+              })
+              .catch(console.error)
+          }}
         >
           {/* <Image
             src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg2%2F20090811%2Fugly_people_23.jpg&f=1&nofb=1"
@@ -48,20 +96,32 @@ export const Member = () => {
           /> */}
 
           <motion.div
-            animate={animControls}
-            initial={{x: 0, y: 0}}
-            className="absolute -left-[35%] -top-10 bg-blue-600  z-50 border-b-[30px] border-l-[150px] border-r-[150px] border-red"
+            animate={animControls[0]}
+            initial={{ x: 100, rotate: -30 }}
+            className="absolute -right-[40%] -bottom-[20%] bg-blue-600 h-[80%] w-[100%] z-50 -rotate-30"
+            onAnimationComplete={() => setAnim(false)}
+          />
+          <motion.div
+            animate={animControls[1]}
+            initial={{ x: -100, rotate: -45 }}
+            className="absolute -left-[50%] bottom-[50%] bg-blue-600 h-[100%] w-[100%] z-50 -rotate-45"
+            onAnimationComplete={() => setAnim(false)}
+          />
+          <motion.div
+            animate={animControls[2]}
+            initial={{ x: -100, rotate: -45, skew: '45deg' }}
+            className="absolute -left-[5%] bottom-[5%] bg-white h-[20%] w-[80%] z-50 -rotate-45 rounded-md"
+            onAnimationComplete={() => setAnim(false)}
+          />
+          <motion.div
+            animate={animControls[3]}
+            initial={{ x: -100, rotate: -45, skew: '45deg' }}
+            className="absolute -right-[40%] top-0 bg-yellow-300 h-[20%] w-[45%] z-50 rotate-45 rounded-md"
             onAnimationComplete={() => setAnim(false)}
           />
           {/* <motion.div
             animate={animControls}
-            // initial={{x: 10}}
-            className="absolute -right-[20%] -bottom-10 bg-blue-600 h-[60%] w-[100%] z-50 -rotate-45"
-            onAnimationComplete={() => setAnim(false)}
-          /> */}
-          {/* <motion.div
-            animate={animControls}
-            className="absolute right-[596px] bg-blue-600 h-[100px] w-24 z-50 rotate-45"
+            className="absolute right-[596px] bg-blue-400 h-[100px] w-24 z-50 rotate-60"
             onAnimationComplete={() => setAnim(false)}
           /> */}
         </div>
