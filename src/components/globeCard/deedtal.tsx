c0e8b3424fe64a979cb4f67371a9d1a7114eef56
tsengegimnida <tsengeg65@gmail.com>
2022-05-18 16:17:42 +0800

    globe card

--- src/components/globeCard/deedtal.tsx
+++ src/components/globeCard/deedtal.tsx
@@ -1,12 +1,11 @@
-import { GlobeCard } from "components";
-import { handleIcon } from "utils";
+
 import { Emphasis } from "components";
 
 const Title = () => {
     return (
 
         <div className="mb-[120px] mobile:mb-[66px] text-center ">
-            <Emphasis>
+            <Emphasis className={""}>
                 <p className="text-primary-white mobile:text-[26px] mobile:mt-[8px]">What are we doing</p>
             </Emphasis>
         </div>
