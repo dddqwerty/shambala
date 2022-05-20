155b00e0ffca574e4eeb6025ba1cb6f70180635e
Tsogt <84776449+TsogtOnCrack@users.noreply.github.com>
2022-05-20 19:56:06 +0800

    animation are done! (#28)
    Co-authored-by: Tsogt <Tsogt@LEAP-23s-iMac.local>

---
+++ src/pages/naadam.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainLayout } from 'layout'
+
+const Naadam = () => {
+  return (
+    <MainLayout>
+      <div className=" h-[200px] w-[500px] bg-yellow-400"></div>
+      <div>Hello Naadam</div>
+    </MainLayout>
+  )
+}
+
+export default Naadam
