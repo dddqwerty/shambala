650fcae66371e986f105cdc6cec45af9454e9ce4
Honi <tselmuuntsemka1230@gmail.com>
2022-06-15 19:11:26 +0800

    naadamPage

--- src/components/BigPicture.jsx
+++ src/components/BigPicture.jsx
@@ -1,7 +1,7 @@
 export const BigPicture = () => {
   return (
     <div className="max-w-shambWidth w-full flex justify-center items-center z-10">
-      <img src="assets/bigPictureSH.png" className="w-full md:w-[90%]" alt="" />
+      <img src="assets/bigPictureSH.jpg" className="w-full md:w-[90%]" alt="" />
     </div>
   )
 }
--- src/components/Pictures/cells.jsx
+++ src/components/Pictures/cells.jsx
@@ -3,14 +3,14 @@ import Typography from 'components/typography'
 export const Cells = (props) => {
   const { text, pic, className } = props
   return (
-    <div className={`${className} w-72 h-72`}>
-      <img src={pic} className="absolute w-72 h-72" alt="" />
-      <div
-        id="haa"
-        className="absolute z-10 hover:cursor-pointer w-72 h-72 hover:bg-black/50 text-transparent hover:text-white/50 flex justify-center items-center"
+    <div className={`group relative flex justify-center items-center ${className}`}>
+      <img src={pic} alt="" className="group-hover:opacity-[.56] duration-300" />
+      <Typography
+        variant="body-web"
+        className="text-[#C0C0C0] absolute bottom-50% w-[60%] group-hover:block hidden duration-300 text-[11px] lg:text-[16px]"
       >
-        <Typography className={'w-[80%]'} children={text} variant={'body-mobile'} />
-      </div>
+        {text}
+      </Typography>
     </div>
   )
 }
