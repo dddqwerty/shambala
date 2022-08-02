974b476f43c2a32e1972cf4d24d62a84e0bf7e94
TsogtOnCrack <tsogt.otgo@gmail.com>
2022-08-02 15:22:28 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import Fournumber from 'components/telmun/hedentoo'
 import FirstPart from 'layout/indexPageComponents/first'
 import OurGoalsPart from 'layout/indexPageComponents/ourGoalsPart'
 import Titi from 'layout/bobo'
-import CardWithIconn from './style/cardWithIcon'
+import CardWithIconn from '../components/style/cardWithIcon'
 
 import { Plans } from 'components/Plans'
 import { Info } from 'components'
-import Whatarewedoing from './style/globeCard'
+import Whatarewedoing from '../components/style/globeCard'
 import Content from '../../public/assets/data.js'
 
 const data = Content.index
